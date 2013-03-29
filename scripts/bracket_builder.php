b50e02795b717bf76aec12efd184540c5a5f924f
Will Koper <koperw@washpost.com>
2013-03-29 16:46:13 -0400

    almost ready to watch some real scores

--- scripts/bracket_builder.php
+++ scripts/bracket_builder.php
@@ -8,10 +8,15 @@ function jsonp_decode($jsonp, $assoc = false) { // PHP 5.3 adds depth as third p
     return json_decode(trim($jsonp,'();'), $assoc);
 }
 function read_game_data(){
+	//Get the database ready
+	$insertion = prepare_to_insert();
+	//get the game data
 	$raw_data = file_get_contents('http://data.ncaa.com/jsonp/gametool/brackets/championships/basketball-men/d1/2012/data.json?callback=foo');
 	echo("data fetched. parsing ....");
 	$data = jsonp_decode($raw_data, true);
-	//var_dump($data);
+	
+
+
 	foreach($data["games"] as $game){
 		//reset variables
 		$underdog = null;
@@ -29,31 +34,39 @@ function read_game_data(){
 			//find out who the underdog is
 			if ($game["away"]["isTop"] === "F"){
 				$underdog = "away";
+				$fave = "home";
 			}
 			else if($game["home"]["isTop"] === "F"){
 				$underdog = "home";
+				$fave = "away";
 			}
 			$gameStart =$game["gameDate"].' '.$game['startTime'];
-			$gameStart = str_replace('ET', 'EST', $gamestart);
+			$gameStart = str_replace('ET', 'EST', $gameStart);
 			$gameDT = new DateTime($gameStart);
 			$fmtGameStart = $gameDT->format('Y-m-d H:i');
 			$gameData = array(
 				":gameID" => $game["contestId"],
 				":game_start" => $fmtGameStart,
-				":under_dog" => $underdog);
+				":under_dog" => $underdog,
+				":fave" => $fave);
 			var_dump($gameData);
-			add_game_to_matchups($gameData);
+			add_game_to_matchups($gameData, $insertion);
 		}
 	}
 
 }
-function add_game_to_matchups($gameArray){
+function prepare_to_insert(){
+	$pdo = db_handle(); //defined in db_config.php just a pdo object.
+	$sql = 'insert into matchups values(:gameID, :game_start, :under_dog, :fave)';
+	$smt = $pdo->prepare($sql);
+	return $smt;
+
+}
+function add_game_to_matchups($gameArray, $smt){
 	//insert game into table
 	//script should only run once a week, so don't worry about updates
 	
-	$pdo = db_handle(); //defined in db_config.php just a pdo object.
-	$sql = 'insert into matchups values(:gameID, :game_start, :under_dog)';
-	$smt = $pdo->prepare($sql);
+	
 	$inserted = $smt->execute($gameArray);
 	if($inserted !== false){
 		echo "game added!";
---
+++ scripts/scorechecker.php
@@ -0,0 +1,57 @@
+<?php
+$script_start = time();
+include ("db_config.php");
+
+function get_games_list(){
+	$pdo = db_handle();
+	$smt = $pdo->query("SELECT gameID, under_dog from today_matchups");
+	$results = $smt->fetchall(PDO::FETCH_ASSOC);
+	$gameDict = array();
+	foreach($results as $game){
+		$gameDict[$game["gameID"]] = $game["under_dog"];
+	}
+	return $gameDict;
+}
+function jsonp_decode($jsonp, $assoc = false) { // PHP 5.3 adds depth as third parameter to json_decode
+    if($jsonp[0] !== '[' && $jsonp[0] !== '{') { // we have JSONP
+       $jsonp = substr($jsonp, strpos($jsonp, '('));
+    }
+    return json_decode(trim($jsonp,'();'), $assoc);
+}
+function read_game_data(){
+	//get the game data
+	$url = 'http://data.ncaa.com/jsonp/gametool/brackets/championships/basketball-men/d1/2012/data.json?callback=foo';
+	// $url = '../data/dummydata.json';
+
+	$raw_data = file_get_contents($url);
+	echo("data fetched. parsing ....");
+	$data = jsonp_decode($raw_data, true);
+	$gamesToWatch = get_games_list();
+	$output = array();
+	//build asscociative array from gameids
+	foreach ($data["games"] as $game){
+		$gameID = $game["contestId"];
+		if (array_key_exists($gameID, $gamesToWatch)){
+			$minsLeft = substr($gamep["timeclock"]);
+			if($minsLeft <5){
+				$fave = $gamesToWatch[$gameID]["fave"];
+				$faveScore = $game[$fave]["score"];
+				$dog = $gamesToWatch[$gameID]["under_dog"];
+				$dogScore = $game[$dog]["score"];
+				if($faveScore - $dogScore < 10){
+					$output[$gameID] = $game;
+				}
+
+
+			}
+			
+		}
+		
+
+	}
+	return $output;
+}
+$games = read_game_data();
+$elapsed = time() - $script_start;
+var_dump($games);
+echo $elapsed;
